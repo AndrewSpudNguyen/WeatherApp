0d5e4bf4d10f4c57393c20ee9304ff5da29095ed
Andrew Nguyen <andrew.spud49@gmail.com>
2021-08-03 14:25:45 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Andrew Nguyen on 2021-08-03.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
